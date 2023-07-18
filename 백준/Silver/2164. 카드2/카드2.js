5c73c3645069dfa8baa17f73ad891be2c311d8e8
seom98 <119838763+seom98@users.noreply.github.com>
2023-07-18 20:06:15 +0900

    [Silver IV] Title: 카드2, Time: 128 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.js
@@ -0,0 +1,21 @@
+    const fs = require('fs');
+    const input = fs.readFileSync('/dev/stdin').toString();
+    let num = Number(input);
+    if (num == 1) {
+        console.log(1)
+    } else if (num == 2) {
+        console.log(2)
+    } else {
+        let i = 1
+        while (true) {
+            if (2 ** i < num && num <= 2 ** (i+1)) {
+                num = num - (2 ** i)
+                num = num * 2
+                console.log(num)
+                break;
+            }
+            else {
+                i++
+            }
+        }
+    }
