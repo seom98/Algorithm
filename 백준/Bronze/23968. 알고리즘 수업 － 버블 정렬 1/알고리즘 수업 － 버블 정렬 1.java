8d7b9942b41ebe9324d53912446e17655ecd2ae8
seom98 <119838763+seom98@users.noreply.github.com>
2023-07-25 23:17:43 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 600 ms, Memory: 27832 KB -BaekjoonHub

---
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        int count = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 1; i < num; i++) {
+            for(int j = 0; j < num - i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp;
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    count--;
+                    if (count == 0) {
+                        System.out.print(arr[j] + " " + arr[j+1]);
+                        break;
+                    }
+                }
+            }
+        }
+        if (count > 0) {
+            System.out.print(-1);
+        }
+    }
+}
