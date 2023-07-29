8cba9758d6f646a6d294f18400933b417dbf33e3
seom98 <119838763+seom98@users.noreply.github.com>
2023-07-30 00:05:40 +0900

    [Silver IV] Title: 좋은 단어, Time: 284 ms, Memory: 19448 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -1,14 +1,10 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Stack;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int num = Integer.parseInt(br.readLine());
         Stack<Character> stack = new Stack<>();
@@ -19,23 +15,14 @@ public class Main {
             if (goodString.length() % 2 == 0) {
 
                 for (int j = 0; j < goodString.length(); j++) {
-                    char goodPeek = stack.isEmpty() ? 'F' : stack.peek();
+                    char goodPeek = stack.isEmpty() ? '놉' : stack.peek();
                     char goodPush = stack.push(goodString.charAt(j));
-
-                    if (goodPush == goodPeek) {
-                        stack.pop(); stack.pop();
-                    }
-
+                    if (goodPush == goodPeek) { stack.pop(); stack.pop(); }
                 }
-                if (stack.isEmpty())
-                    cnt++;
-                stack.clear();
+                if (stack.isEmpty()) cnt++;
+                else stack.clear();
             }
         }
-
-        bw.write(String.valueOf(cnt));
-
-        br.close();
-        bw.close();
+        System.out.print(cnt);
     }
 }
