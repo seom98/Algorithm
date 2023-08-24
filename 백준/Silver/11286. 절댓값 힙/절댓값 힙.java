d9e6cde6fb2041320e3ed959a6778ac05ec575db
seom98 <119838763+seom98@users.noreply.github.com>
2023-08-24 22:48:16 +0900

    [Silver I] Title: 절댓값 힙, Time: 304 ms, Memory: 26816 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+        	@Override
+        	public int compare(Integer o1, Integer o2) {
+                if(Math.abs(o1) < Math.abs(o2)) {
+                    return -1;
+                }else if(Math.abs(o1) > Math.abs(o2)) {
+                    return 1;
+                }else {
+                	if (o1 < o2) {
+                		return -1;
+                	} else {
+                		return 1;
+                	}
+                }
+            }
+		});
+
+        for (int i = 0; i < N; i++) {
+            int cmd = Integer.parseInt(br.readLine());
+            if (cmd != 0) {
+                pq.add(cmd);
+            } else {
+                if (pq.isEmpty()) {
+                    bw.write("0\n");
+                } else {
+                    bw.write(pq.poll() + "\n");
+                }
+            }
+        }
+
+        br.close();
+        bw.close();
+    }
+}
