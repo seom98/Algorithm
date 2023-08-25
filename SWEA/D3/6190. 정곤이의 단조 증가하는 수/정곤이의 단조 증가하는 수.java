fd885971fb38f89b69e728633010d7f449a56402
seom98 <119838763+seom98@users.noreply.github.com>
2023-08-25 11:57:37 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 1,175 ms, Memory: 166,188 KB -BaekjoonHub

---
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); //테스트케이스의 개수.
+         
+        for (int tc = 1; tc<=t; tc++) {
+        	int N = sc.nextInt();
+        	int[] arr = new int[N];
+        	
+        	for (int i = 0; i < N; i++) {
+        		arr[i] = sc.nextInt();
+        	}
+        	
+        	int max = -1; //곱한값의 최댓값. 
+        	for (int i = 0; i < N-1; i++) {
+        		for (int k = i+1; k < N; k++) {
+        			int num_multiply = arr[i]*arr[k]; //두수를 곱한값. 
+        			String multiply = String.valueOf(num_multiply); //두수를 곱한값을 스트링으로 변경.
+        			boolean chack = true; //단조증가인지 확인.
+        			for (int j = 0; j < multiply.length()-1; j++) { //스트링의 길이-1만큼 포문.
+        				if (multiply.charAt(j) > multiply.charAt(j+1)) //단조증가가 아니라면 체크를 false로.
+        					chack = false;
+        			}
+        			if (chack && max < num_multiply) { //단조증가이면서 최댓값보다 큰값이 나왔을때. 
+        				max = num_multiply; //최댓값 변경.
+        			}
+        		}
+        	}
+        	System.out.println("#" + tc + " " + max); //출력 
+        }
+        sc.close();
+	}
+}
