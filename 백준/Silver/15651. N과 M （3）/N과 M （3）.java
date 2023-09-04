f359fd821a7ab7f534dc242c55f2ab7de4190bc0
seom98 <119838763+seom98@users.noreply.github.com>
2023-09-04 21:26:29 +0900

    [Silver III] Title: N과 M (3), Time: 480 ms, Memory: 114880 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static int[] nums;// 배열
+	public static int N, M; // 원소수
+	public static int[] result; // 결과저장
+	public static boolean[] visited; // 해당 원소 사용 유무
+	public static StringBuilder sb;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		nums = new int[N];
+		for (int i = 1; i <= N; i++) {
+			nums[i - 1] = i;
+		}
+		M = sc.nextInt();
+		result = new int[M];
+		visited = new boolean[N];
+		sb = new StringBuilder();
+		perm(0);
+		System.out.println(sb);
+		sc.close();
+	}
+
+	public static void perm(int idx) {
+		if (idx == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(result[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			result[idx] = nums[i];
+			perm(idx + 1);
+		}
+	}
+}
