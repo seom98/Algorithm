bcb1816e70da054f1402d6f752f168f5128fad2a
seom98 <119838763+seom98@users.noreply.github.com>
2023-09-04 20:28:46 +0900

    [Gold V] Title: CCW, Time: 84 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Gold/11758. CCW/CCW.java
+++ 백준/Gold/11758. CCW/CCW.java
@@ -1,21 +1,26 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[][] a = new int[3][2];
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 2; j++) {
-				a[i][j] = sc.nextInt();
-			}
-		}
-		int x = 0, y = 0;
-		for (int i = 0; i < 3; i++) {
-			x += a[i][0]*a[(i+1)%3][1];
-			y += a[i][1]*a[(i+1)%3][0];
-		}
-		System.out.println(x-y==0 ? 0 : x-y<0 ? -1 : 1);
-		sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] a = new int[3][2];
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                a[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int x = 0, y = 0;
+        for (int i = 0; i < 3; i++) {
+            x += a[i][0] * a[(i+1)%3][1];
+            y += a[i][1] * a[(i+1)%3][0];
+        }
+
+        System.out.println(x-y == 0 ? 0 : x-y < 0 ? -1 : 1);
+        br.close();
+    }
 }
