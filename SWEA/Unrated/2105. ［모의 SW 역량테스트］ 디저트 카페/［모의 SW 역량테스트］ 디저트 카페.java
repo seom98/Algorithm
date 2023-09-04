792cf1d1fafc0f54f49d4d93d183278cd7c57340
seom98 <119838763+seom98@users.noreply.github.com>
2023-09-04 17:15:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 505 ms, Memory: 114,976 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -1,61 +1,66 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-	
+
 	static int N, x, y, sum;
 	static int[][] arr;
-	static int[] dc = {1, -1, -1, 1};
-	static int[] dr = {1, 1, -1, -1};
+	static int[] dc = { 1, -1, -1, 1 };
+	static int[] dr = { 1, 1, -1, -1 };
 	static Set<Integer> set;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+
 		for (int t = 1; t <= T; t++) {
-			N = sc.nextInt();
+			N = Integer.parseInt(br.readLine());
 			arr = new int[N][N];
-			set = new HashSet<Integer>();
+			set = new HashSet<>();
 			sum = -1;
+
 			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
-					arr[i][j] = sc.nextInt();
+					arr[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			
-			for (int i = 0; i < N-2; i++) {
-				for (int j = 1; j < N-1; j++) {
+
+			for (int i = 0; i < N - 2; i++) {
+				for (int j = 1; j < N - 1; j++) {
 					x = i;
 					y = j;
-					dessert(i, j, 0, 0);
+					dessert(i, j, 0, 0, 0);
 					set.clear();
 				}
 			}
-
-			System.out.println("#" + t + " " + sum);
+			bw.write("#" + t + " " + sum + "\n");
 		}
-		sc.close();
+		br.close();
+		bw.close();
 	}
-	
-	static void dessert(int a, int b, int c, int s) {
-		//기저조건
+
+	static void dessert(int a, int b, int c, int s, int n) {
 		if (c == 3 && a == x && b == y) {
-			if (sum < s) sum = s;
+			if (sum < s)
+				sum = s;
 			return;
 		}
 		
-		if (a >= 0 && b >= 0 && a < N && b < N && c < 3 && !set.contains(arr[a][b])) {
+		if (n < 0) return;
+		if (c == 0 || c == 1) n++;
+		else n--;
+
+		if (a >= 0 && b >= 0 && a < N && b < N && !set.contains(arr[a][b])) {
 			set.add(arr[a][b]);
-			dessert(a+dr[c], b+dc[c], c, s+1);
-			dessert(a+dr[c+1], b+dc[c+1], c+1, s+1);
+			if (c < 3) {
+				dessert(a + dr[c], b + dc[c], c, s + 1, n);
+				dessert(a + dr[c + 1], b + dc[c + 1], c + 1, s + 1, n);
+			} else {
+				dessert(a + dr[c], b + dc[c], c, s + 1, n);
+			}
 			set.remove(arr[a][b]);
 		}
-		if (a >= 0 && b >= 0 && a < N && b < N && c == 3 && !set.contains(arr[a][b])) {
-			set.add(arr[a][b]);
-			dessert(a+dr[c], b+dc[c], c, s+1);
-			set.remove(arr[a][b]);
-		}
-		
 	}
 }
