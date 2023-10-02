585c7c06cfab3ed05c982c386c5d8c321937a2e7
seom98 <119838763+seom98@users.noreply.github.com>
2023-10-03 02:11:23 +0900

    [Gold II] Title: 문제집, Time: 1028 ms, Memory: 160272 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		PriorityQueue<Integer> queue = new PriorityQueue<>();
+		StringBuilder sb = new StringBuilder();
+		int V = sc.nextInt();
+		int E = sc.nextInt();
+		List<List<Integer>> adjList = new ArrayList<>();
+		int[] degree = new int[V + 1];
+
+		for (int i = 0; i <= V; i++) {
+			adjList.add(new ArrayList<>());
+		}
+
+		for (int i = 0; i < E; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			adjList.get(A).add(B);
+			degree[B]++;
+		}
+
+		for (int i = 1; i <= V; i++) {
+			if (degree[i] == 0)
+				queue.add(i);
+		}
+
+		while (!queue.isEmpty()) {
+			int work = queue.poll();
+			sb.append(work).append(" ");
+
+			for (int i : adjList.get(work)) {
+				degree[i]--;
+				if (degree[i] == 0)
+					queue.add(i);
+			}
+		}
+		System.out.println(sb);
+
+		sc.close();
+	}
+}
