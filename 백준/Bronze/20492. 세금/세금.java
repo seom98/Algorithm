a1cc427f4d0fc5a9085277c4c39df74fad811412
seom98 <119838763+seom98@users.noreply.github.com>
2023-10-14 20:59:02 +0900

    [Bronze V] Title: 세금, Time: 112 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt()/1000;
+        System.out.println(N*780 + " " + N*956);
+    }
+}
