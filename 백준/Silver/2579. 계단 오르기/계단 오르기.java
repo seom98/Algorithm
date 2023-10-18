2d463028d003333a7d105ba67b111f0ffe71013e
seom98 <119838763+seom98@users.noreply.github.com>
2023-10-18 16:16:20 +0900

    [Silver III] Title: 계단 오르기, Time: 132 ms, Memory: 13224 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 계단개수
+        int[] arr = new int[N + 1]; // 계단점수
+        int[] dp = new int[N + 1]; // 계단당 최대점수
+
+        for (int i = 1; i <= N; i++)
+            arr[i] = sc.nextInt();
+
+        dp[1] = arr[1];
+        
+        if (N >= 2)
+            dp[2] = arr[1] + arr[2];
+
+        for (int i = 3; i <= N; i++)
+            dp[i] = Math.max(dp[i-2], dp[i-3]+arr[i-1]) + arr[i];
+
+        System.out.println(dp[N]);
+        sc.close();
+    }
+}
