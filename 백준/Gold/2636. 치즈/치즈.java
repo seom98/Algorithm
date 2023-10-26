5d75ccbc4513321d1ba6cc317ed11e4cd6752fdc
seom98 <119838763+seom98@users.noreply.github.com>
2023-10-26 10:37:04 +0900

    [Gold IV] Title: 치즈, Time: 116 ms, Memory: 13948 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,23 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+    // 자 일단 그냥 배열이 있고, 카운트를 체크할 배열이 있음.
+    // 만들어진 그냥 배열 너비우선 탐색을 하면서 갔다온 자리는 -1로 표시하고 주변에 치즈칸이 있으면 +1씩 해줌.
+    // 끝까지 돌고 나면 카운트가 +2 이상 된 친구들만 전부 그냥 배열에서 0으로 바꾸는거임!!
     static int[] dRow = {0, 1, 0, -1};
     static int[] dCol = {1, 0, -1, 0};
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         int[][] arr = new int[N][M];
         int count = 0;
         int day = 0;
         int cheese = 0;
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                arr[i][j] = sc.nextInt();
+                arr[i][j] = Integer.parseInt(st.nextToken());
                 if (arr[i][j] == 1)
-                    count++; // 처음에 치즈개수 구해놓고 치즈개수 0이면 멈추게 해야겠다.
+                    count++;
             }
         }
         while (count != 0) {
@@ -43,20 +51,18 @@ public class Main {
                     }
                 }
             }
-            cheese = 0;
+            int temp = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
                     if (C[i][j] > 0) {
                         arr[i][j] = 0;
-                        count--;
-                        cheese++;
+                        temp++;
                     }
                 }
             }
+            count -= temp;
+            cheese = temp;
         }
         System.out.println(day + "\n" + cheese);
-        // 자 일단 그냥 배열이 있고, 카운트를 체크할 배열이 있음.
-        // 만들어진 그냥 배열 너비우선 탐색을 하면서 갔다온 자리는 -1로 표시하고 주변에 치즈칸이 있으면 +1씩 해줌.
-        // 끝까지 돌고 나면 카운트가 +2 이상 된 친구들만 전부 그냥 배열에서 0으로 바꾸는거임!!
     }
 }
